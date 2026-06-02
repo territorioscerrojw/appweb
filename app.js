ad8071ee4e5e1f4710c3f1184c722f3c9611b39c
territorioscerrojw <territorioscerro.jw@gmail.com>
2026-06-02 15:18:10 +0200

    Actualizar app.js

--- app.js
+++ app.js
@@ -329,7 +329,7 @@ div.className = `tarjeta-apple ${esPrio ? 'prioritaria' : ''} ${seleccionadoActi
                 ? `<svg class="svg-icono-mini" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>` 
                 : `<svg class="svg-icono-mini" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>`
               }
-              ${!mapa.trabajado ? "Pendiente" : "Completo"}
+              ${!mapa.trabajado ? "Pendiente" : "Completado"}
             </span>
             ${esPrio ? `<span class="tag-prio-mini">⚠️ PRIORITARIO</span>` : ''}
           </div>
