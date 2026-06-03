def8a19af1f74cbb6fa4e3add2b02d475892493e
territorioscerrojw <territorioscerro.jw@gmail.com>
2026-06-03 13:25:11 +0200

    Actualizar app.js

--- app.js
+++ app.js
@@ -344,8 +344,9 @@ div.className = `tarjeta-apple ${esPrio ? 'prioritaria' : ''} ${seleccionadoActi
       
               // Dentro de la sección 'else' de filtrarYRenderizar:
 div.innerHTML = `
+ ${esPrio ? `<span class="tag-prio-mini">⚠️ PRIORITARIO</span>` : ''}
   <div class="img-lateral-wrapper-rectangular">
-     ${esPrio ? `<span class="tag-prio-mini">⚠️ PRIORITARIO</span>` : ''}
+    
     <button class="btn-lupa-flotante" onclick="abrirVisorPantallaCompleta('${mapa.rutaMapa}', '${parseInt(mapa.id)}', event)">
       <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
     </button>
